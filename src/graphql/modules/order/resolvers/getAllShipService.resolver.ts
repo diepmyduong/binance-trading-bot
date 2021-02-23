6715503cd8bdcd21b88f48a5cd915bdc8739880d
Jack Bereson <jackbereson@gmail.com>
2021-02-23 17:39:44 +0700

    Fix : getallShipService

---
+++ src/graphql/modules/order/resolvers/getAllShipService.resolver.ts
@@ -0,0 +1,29 @@
+import { VietnamPostHelper } from "../../../../helpers/vietnamPost/vietnamPost.helper";
+import { ROLES } from "../../../../constants/role.const";
+import { Context } from "../../../context";
+import { ShipMethod } from "../order.model";
+
+const Query = {
+  getAllShipService: async (root: any, args: any, context: Context) => {
+    context.auth(ROLES.ADMIN_EDITOR);
+
+    const { shipMethod } = args;
+
+    let services: any = [];
+
+    switch (shipMethod) {
+      case ShipMethod.VNPOST:
+        services = await VietnamPostHelper.getListService();
+        break;
+
+      default:
+        services = [];
+        break;
+    }
+
+    // console.log("services", services);
+    return services;
+  },
+};
+
+export default { Query };

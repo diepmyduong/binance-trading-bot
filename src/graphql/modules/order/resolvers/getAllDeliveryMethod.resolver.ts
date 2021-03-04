b247129ae1f5de3eea2cc13465024fc6ceb7dcb5
Jack Bereson <jackbereson@gmail.com>
2021-03-04 10:26:40 +0700

    Fix :  get all delivery method

--- src/graphql/modules/order/resolvers/getAllDeliveryMethod.resolver.ts
+++ src/graphql/modules/order/resolvers/getAllDeliveryMethod.resolver.ts
@@ -1,14 +1,14 @@
 import { SettingKey } from "../../../../configs/settingData";
 import { Context } from "../../../context";
 import { SettingHelper } from "../../setting/setting.helper";
-import { ShipMethod, ShipMethods } from "../order.model";
+import { getShipMethods, ShipMethod } from "../order.model";
 
 const Query = {
   getAllDeliveryMethod: async (root: any, args: any, context: Context) => {
     const [enabledVNPost] = await SettingHelper.loadMany([
       SettingKey.DELIVERY_ENABLED_VNPOST,
     ]);
-    const shipMethods = ShipMethods;
+    const shipMethods = await getShipMethods();
     const results = [];
     results.push(shipMethods.find((ship) => ship.value === ShipMethod.NONE));
     results.push(shipMethods.find((ship) => ship.value === ShipMethod.POST));

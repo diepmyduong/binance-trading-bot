8e4cd0c6f8a214a48afcf24f770f4a43fd7972e9
Jack Bereson <jackbereson@gmail.com>
2021-03-09 19:20:29 +0700

    Fix : commission3

--- src/scheduler/jobs/collaborator.job.ts
+++ src/scheduler/jobs/collaborator.job.ts
@@ -4,6 +4,7 @@ import { CollaboratorModel } from "../../graphql/modules/collaborator/collaborat
 import { SettingKey } from "../../configs/settingData";
 import { SettingHelper } from "../../graphql/modules/setting/setting.helper";
 import { Agenda } from "../agenda";
+import { CustomerModel } from "../../graphql/modules/customer/customer.model";
 
 export class CollaboratorJob {
   static jobName = "Collaborator";
@@ -11,8 +12,7 @@ export class CollaboratorJob {
     return Agenda.create(this.jobName, data);
   }
   static async execute(job: Job, done: any) {
-    // await doBusiness();
-
+    await doBusiness();
     return done();
   }
 }
@@ -26,11 +26,34 @@ const doBusiness = async () => {
     $or: [{ customerId: { $exists: false } }, { customerId: null }],
   }).limit(1000);
 
-  
+  console.log('collaborators',collaborators);
 
+  const collaboratorPhones = collaborators.map((col) => col.phone);
+
+  const customers = await CustomerModel.find({
+    phone: { $in: collaboratorPhones },
+  });
+  // console.log("members", memberss);
+
+  for (const customer of customers) {
+    const collaborator = collaborators.find(
+      (col) => col.phone === customer.phone
+    );
+    if (collaborator) {
+      await CollaboratorModel.findByIdAndUpdate(
+        collaborator.id,
+        {
+          $set: {
+            customerId: customer._id,
+          },
+        },
+        { new: true }
+      );
+    }
+  }
 };
 
-(async () => {
-  console.log("test businessssssssssssssssssssssss");
-  await doBusiness();
-})();
+// (async () => {
+//   console.log("test businessssssssssssssssssssssss");
+//   await doBusiness();
+// })();
--- src/scheduler/repeat.ts
+++ src/scheduler/repeat.ts
@@ -2,11 +2,16 @@
 
 import moment from "moment-timezone";
 import LuckyWheelJob from "./jobs/luckyWheel.job";
+import CollaboratorJob from "./jobs/collaborator.job";
 
 export function InitRepeatJobs() {
   console.log("Generate Repeat Jobs");
   LuckyWheelJob.create({})
     .repeatEvery("5 seconds")
     .unique({ name: LuckyWheelJob.jobName })
+    .save();
+  CollaboratorJob.create({})
+    .repeatEvery("5 minutes")
+    .unique({ name: CollaboratorJob.jobName })
     .save();
 }

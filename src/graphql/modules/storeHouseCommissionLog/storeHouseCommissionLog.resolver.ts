8544a0d6caa59b56e03d2f9e32e9a433f5fafd1a
Jack Bereson <jackbereson@gmail.com>
2021-03-10 00:17:06 +0700

    Fix : storehouse commission log

--- src/graphql/modules/storeHouseCommissionLog/storeHouseCommissionLog.resolver.ts
+++ src/graphql/modules/storeHouseCommissionLog/storeHouseCommissionLog.resolver.ts
@@ -1,11 +1,19 @@
+import { set } from "lodash";
+import { GraphQLHelper } from "../../../helpers/graphql.helper";
 import { ROLES } from "../../../constants/role.const";
 import { AuthHelper } from "../../../helpers";
 import { Context } from "../../context";
+import { MemberHelper } from "../member/member.helper";
+import { OrderLoader } from "../order/order.model";
 import { storeHouseCommissionLogService } from "./storeHouseCommissionLog.service";
 
 const Query = {
   getAllStoreHouseCommissionLog: async (root: any, args: any, context: Context) => {
-    AuthHelper.acceptRoles(context, [ROLES.ADMIN, ROLES.EDITOR]);
+    AuthHelper.acceptRoles(context, ROLES.ADMIN_EDITOR_MEMBER);
+    const memberHelper = await MemberHelper.fromContext(context);
+    if (memberHelper) {
+      set(args, "q.filter.memberId", memberHelper.member._id);
+    }
     return storeHouseCommissionLogService.fetch(args.q);
   },
   getOneStoreHouseCommissionLog: async (root: any, args: any, context: Context) => {
@@ -24,7 +32,7 @@ const Mutation = {
 };
 
 const StoreHouseCommissionLog = {
-  
+  order: GraphQLHelper.loadById(OrderLoader, "orderId"),
 };
 
 export default {

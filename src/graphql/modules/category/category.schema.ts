206a801fbbef57474152a15619475911d47d3d8e
Jack Bereson <jackbereson@gmail.com>
2021-04-01 16:14:58 +0700

    Fix : getFilteringCategories

--- src/graphql/modules/category/category.schema.ts
+++ src/graphql/modules/category/category.schema.ts
@@ -4,6 +4,7 @@ const schema = gql`
   extend type Query {
     getAllCategory(q: QueryGetListInput): CategoryPageData
     getOneCategory(id: ID!): Category
+    getFilteringCategories: [Category]
   }
 
   extend type Mutation {

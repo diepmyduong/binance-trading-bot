461d1243a98b7fe9eb95b416e4b2a8b9d1b6c730
Jack Bereson <jackbereson@gmail.com>
2021-04-06 11:39:44 +0700

    Fix: report with member

--- src/graphql/modules/report/report.helper.ts
+++ src/graphql/modules/report/report.helper.ts
@@ -10,7 +10,7 @@ import { CustomerModel } from "../customer/customer.model";
 import { IOrder, OrderModel, OrderStatus, ShipMethod } from "../order/order.model";
 import { CommissionLogModel } from "../commissionLog/commissionLog.model";
 import { MemberModel, MemberType } from "../member/member.model";
-import { OrderLogModel } from "../orderLog/orderLog.model";
+import { IOrderLog, OrderLogModel } from "../orderLog/orderLog.model";
 
 export class ReportHelper {
 
@@ -146,22 +146,16 @@ export class ReportHelper {
       set($logMatch, "createdAt.$lte", $lte);
     }
 
-    // console.log("getOrdersStats $match", $match);
-    // console.log("getOrdersStats $logMatch", $logMatch);
+    console.log("getOrdersStats $match", $match);
+    console.log("getOrdersStats $logMatch", $logMatch);
 
     const orderLogs = await OrderLogModel.find($logMatch);
-    const orderIds = orderLogs.map(o => new ObjectId(o.orderId));
-    // console.log("orderIds", orderIds);
-
-    const orders = await OrderModel.find({ _id: { $in: orderIds } });
-    // console.log("orders", orders);
-
-    const allIncomeStats = ReportHelper.getAllIncomeStats(orders);
+    const allIncomeStats = await ReportHelper.getAllIncomeStats(orderLogs);
     // const noneIncomeStats = getNoneIncomeOrderStats(orders);
     // const postIncomeStats = getPostIncomeOrderStats(orders);
     // const vnportIncomeStats = getVNPORTIncomeOrderStats(orders);
 
-    const allCommissionStats = ReportHelper.getAllCommissionStats(orders, addressDeliverys, addressStorehouses);
+    const allCommissionStats = await ReportHelper.getAllCommissionStats(orderLogs, addressDeliverys, addressStorehouses);
     // const noneIncomeStats = getNoneIncomeOrderStats(orders);
     // const postIncomeStats = getPostIncomeOrderStats(orders);
     // const vnportIncomeStats = getVNPORTIncomeOrderStats(orders);
@@ -173,16 +167,26 @@ export class ReportHelper {
   }
 
   // nhan hang tai bc
-  static getAllIncomeStats = (orders: IOrder[]) => {
+  static getAllIncomeStats = async (orderLogs: IOrderLog[]) => {
 
-    const allOrders = ReportHelper.getOrderStats(orders);
-    const pendingOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.PENDING));
-    const confirmedOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.CONFIRMED));
-    const deliveringOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.DELIVERING));
-    const estimatedOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => [OrderStatus.DELIVERING, OrderStatus.PENDING, OrderStatus.CONFIRMED].includes(o.status)));
-    const completedOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.COMPLETED));
-    const failureOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.FAILURE));
-    const canceledOrders = ReportHelper.getOrderStats(orders.filter((o: IOrder) => o.status === OrderStatus.CANCELED));
+    const [allOrders,
+      pendingOrders,
+      confirmedOrders,
+      deliveringOrders,
+      estimatedOrders,
+      completedOrders,
+      failureOrders,
+      canceledOrders] = await Promise.all([
+        ReportHelper.getOrderStats(orderLogs),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.PENDING)),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.CONFIRMED)),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.DELIVERING)),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => [OrderStatus.DELIVERING, OrderStatus.PENDING, OrderStatus.CONFIRMED].includes(o.orderStatus))),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.COMPLETED)),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.FAILURE)),
+        ReportHelper.getOrderStats(orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.CANCELED))
+      ]);
+
 
     return {
       allOrders,
@@ -194,25 +198,29 @@ export class ReportHelper {
       failureOrders,
       canceledOrders,
     }
-  }
+  };
 
   // nhan hang tai bc
-  static getNoneIncomeOrderStats = (orders: IOrder[]) => {
-    return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.NONE));
-  }
+  // static getNoneIncomeOrderStats = (orders: IOrder[]) => {
+  //   return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.NONE));
+  // }
 
-  // nhan hang tai bc
-  static getPostIncomeOrderStats = (orders: IOrder[]) => {
-    return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.POST));
-  }
+  // // nhan hang tai bc
+  // static getPostIncomeOrderStats = (orders: IOrder[]) => {
+  //   return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.POST));
+  // }
 
-  // giao hang tai dia chi
-  static getVNPORTIncomeOrderStats = (orders: IOrder[]) => {
-    return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.VNPOST));
-  }
+  // // giao hang tai dia chi
+  // static getVNPORTIncomeOrderStats = (orders: IOrder[]) => {
+  //   return ReportHelper.getAllIncomeStats(orders.filter((o: IOrder) => o.shipMethod === ShipMethod.VNPOST));
+  // }
 
 
-  static getOrderStats = (orders: IOrder[]) => {
+  static getOrderStats = async (orderLogs: IOrderLog[]) => {
+    const orderIds = orderLogs.length > 0 ? orderLogs.map(o => new ObjectId(o.orderId)) : [];
+    // console.log("orderIds", orderIds);
+    const orders = await OrderModel.find({ _id: { $in: orderIds } });
+    // console.log("orders", orders.length);
     const count = orders.length;
     const sum = count > 0 ? orders.reduce((total: number, o: IOrder) => total += o.amount, 0) : 0;
     return {
@@ -222,22 +230,36 @@ export class ReportHelper {
   }
 
 
-  static getAllCommissionStats = (orders: IOrder[], addressDeliverys: IAddressDelivery[], addressStorehouses: IAddressStorehouse[]) => {
-    const pendingFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.PENDING);
-    const confirmedFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.CONFIRMED);
-    const deliveringFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.DELIVERING)
-    const estimatedFilteredOrders = orders.filter((o: IOrder) => [OrderStatus.DELIVERING, OrderStatus.PENDING, OrderStatus.CONFIRMED].includes(o.status))
-    const completedFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.COMPLETED)
-    const failureFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.FAILURE)
-    const canceledFilteredOrders = orders.filter((o: IOrder) => o.status === OrderStatus.CANCELED)
-    const allOrders = ReportHelper.getCommissionStats(orders, addressDeliverys, addressStorehouses);
-    const pendingOrders = ReportHelper.getCommissionStats(pendingFilteredOrders, addressDeliverys, addressStorehouses);
-    const confirmedOrders = ReportHelper.getCommissionStats(confirmedFilteredOrders, addressDeliverys, addressStorehouses);
-    const deliveringOrders = ReportHelper.getCommissionStats(deliveringFilteredOrders, addressDeliverys, addressStorehouses);
-    const estimatedOrders = ReportHelper.getCommissionStats(estimatedFilteredOrders, addressDeliverys, addressStorehouses);
-    const completedOrders = ReportHelper.getCommissionStats(completedFilteredOrders, addressDeliverys, addressStorehouses);
-    const failureOrders = ReportHelper.getCommissionStats(failureFilteredOrders, addressDeliverys, addressStorehouses);
-    const canceledOrders = ReportHelper.getCommissionStats(canceledFilteredOrders, addressDeliverys, addressStorehouses);
+  static getAllCommissionStats = async (orderLogs: IOrderLog[], addressDeliverys: IAddressDelivery[], addressStorehouses: IAddressStorehouse[]) => {
+
+    const pendingFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.PENDING);
+    const confirmedFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.CONFIRMED);
+    const deliveringFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.DELIVERING)
+    const estimatedFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => [OrderStatus.DELIVERING, OrderStatus.PENDING, OrderStatus.CONFIRMED].includes(o.orderStatus))
+    const completedFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.COMPLETED)
+    const failureFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.FAILURE)
+    const canceledFilteredOrderLogs = orderLogs.filter((o: IOrderLog) => o.orderStatus === OrderStatus.CANCELED)
+
+
+    const [
+      allOrders,
+      pendingOrders,
+      confirmedOrders,
+      deliveringOrders,
+      estimatedOrders,
+      completedOrders,
+      failureOrders,
+      canceledOrders,
+    ] = await Promise.all([
+      ReportHelper.getCommissionStats(orderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(pendingFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(confirmedFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(deliveringFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(estimatedFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(completedFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(failureFilteredOrderLogs, addressDeliverys, addressStorehouses),
+      ReportHelper.getCommissionStats(canceledFilteredOrderLogs, addressDeliverys, addressStorehouses),
+    ]);
 
     return {
       allOrders,
@@ -251,8 +273,13 @@ export class ReportHelper {
     }
   }
 
-  static getCommissionStats = (orders: IOrder[], addressDeliverys: IAddressDelivery[], addressStorehouses: IAddressStorehouse[]) => {
-    const count = orders.length;
+  static getCommissionStats = async (orderLogs: IOrderLog[], addressDeliverys: IAddressDelivery[], addressStorehouses: IAddressStorehouse[]) => {
+    const orderIds = orderLogs.map(o => new ObjectId(o.orderId));
+    // console.log("orderIds", orderIds);
+    const orders = await OrderModel.find({ _id: { $in: orderIds } });
+
+    const count = orderIds.length;
+
     const totalCommission1 = count > 0 ? ReportHelper.getCommission1FromOrder(orders) : 0;
     const totalCommission2 = count > 0 ? ReportHelper.getCommission2FromOrder(orders) : 0;
     const totalCommission3 = count > 0 ? ReportHelper.getCommission3FromOrder(orders, addressDeliverys, addressStorehouses) : 0;

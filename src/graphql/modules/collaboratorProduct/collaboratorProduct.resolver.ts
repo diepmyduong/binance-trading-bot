c299fcf17ad420514a53589f42cb4bfac75c44d3
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-05-18 17:37:47 +0700

    Fix: Đổi link chia sẻ sản phẩm qua webapp domain

--- src/graphql/modules/collaboratorProduct/collaboratorProduct.resolver.ts
+++ src/graphql/modules/collaboratorProduct/collaboratorProduct.resolver.ts
@@ -33,30 +33,30 @@ const Mutation = {
     const { data } = args;
     const { collaboratorId, productId } = data;
 
-    const host = await SettingHelper.load(SettingKey.APP_DOMAIN);
+    const host = await SettingHelper.load(SettingKey.WEBAPP_DOMAIN);
     const secret = `${collaboratorId}-${productId}`;
 
     let shortCode = KeycodeHelper.alpha(secret, 6);
     let shortUrl = `${host}/san-pham/${shortCode}`;
 
     const collaboratorProduct = await CollaboratorProductModel.find({ collaboratorId, productId });
-    if(collaboratorProduct.length > 0){
+    if (collaboratorProduct.length > 0) {
       throw ErrorHelper.mgQueryFailed("Link sản phẩm đã tồn tại");
     }
     // console.log('collaboratorId', collaboratorId);
     const collaborator = await CollaboratorModel.findById(collaboratorId);
     // console.log('collaborator', collaborator);
 
-    if(!collaborator){
+    if (!collaborator) {
       throw ErrorHelper.mgRecoredNotFound("Cộng tác viên");
     }
 
     const product = await ProductModel.findById(productId);
-    if(!product){
+    if (!product) {
       throw ErrorHelper.mgRecoredNotFound("Sản phẩm");
     }
 
-    if(!product.isPrimary){
+    if (!product.isPrimary) {
       throw ErrorHelper.somethingWentWrong("Sản phẩm phải là sản phẩm của bưu cục");
     }
 
@@ -70,14 +70,9 @@ const Mutation = {
     data.shortUrl = shortUrl;
     data.shortCode = shortCode;
 
-
     return await collaboratorProductService.create(data);
   },
-  deleteOneCollaboratorProduct: async (
-    root: any,
-    args: any,
-    context: Context
-  ) => {
+  deleteOneCollaboratorProduct: async (root: any, args: any, context: Context) => {
     AuthHelper.acceptRoles(context, ROLES.ADMIN_EDITOR_MEMBER_CUSTOMER);
     const { id } = args;
     return await collaboratorProductService.deleteOne(id);

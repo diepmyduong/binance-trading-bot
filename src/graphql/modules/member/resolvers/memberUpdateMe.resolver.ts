4e12a2412cb3cd9031a28987297d3f801105b61c
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-05-26 16:45:33 +0700

    Fix: [production] Đặt hàng bị báo lỗi

--- src/graphql/modules/member/resolvers/memberUpdateMe.resolver.ts
+++ src/graphql/modules/member/resolvers/memberUpdateMe.resolver.ts
@@ -3,7 +3,7 @@ import { AuthHelper } from "../../../../helpers";
 import { Context } from "../../../context";
 import { AddressDeliveryModel } from "../../addressDelivery/addressDelivery.model";
 import { AddressStorehouseModel } from "../../addressStorehouse/addressStorehouse.model";
-import { MemberModel } from "../member.model";
+import { MemberLoader, MemberModel } from "../member.model";
 
 const Mutation = {
   memberUpdateMe: async (root: any, args: any, context: Context) => {
@@ -13,35 +13,43 @@ const Mutation = {
       context.tokenData._id,
       { $set: data },
       { new: true }
-    ).then(async (updated) => {
-      const member = await MemberModel.findById(context.tokenData._id);
+    ).then(async (member) => {
+      MemberLoader.clear(member.id);
       await Promise.all([
-        AddressDeliveryModel.findOneAndUpdate({ code: member.code }, {
-          $set: {
-            phone: member.phone,
-            address: member.address,
-            district: member.district,
-            districtId: member.districtId,
-            ward: member.ward,
-            wardId: member.wardId,
-            province: member.province,
-            provinceId: member.provinceId,
-            name: member.shopName,
-          }
-        }, { new: true }),
-        AddressStorehouseModel.findOneAndUpdate({ code: member.code }, {
-          $set: {
-            phone: member.phone,
-            address: member.address,
-            district: member.district,
-            districtId: member.districtId,
-            ward: member.ward,
-            wardId: member.wardId,
-            province: member.province,
-            provinceId: member.provinceId,
-            name: member.shopName,
-          }
-        }, { new: true })
+        AddressDeliveryModel.updateOne(
+          { code: member.code },
+          {
+            $set: {
+              phone: member.phone,
+              address: member.address,
+              district: member.district,
+              districtId: member.districtId,
+              ward: member.ward,
+              wardId: member.wardId,
+              province: member.province,
+              provinceId: member.provinceId,
+              name: member.shopName,
+            },
+          },
+          { new: true }
+        ),
+        AddressStorehouseModel.updateOne(
+          { code: member.code },
+          {
+            $set: {
+              phone: member.phone,
+              address: member.address,
+              district: member.district,
+              districtId: member.districtId,
+              ward: member.ward,
+              wardId: member.wardId,
+              province: member.province,
+              provinceId: member.provinceId,
+              name: member.shopName,
+            },
+          },
+          { new: true }
+        ),
       ]);
     });
   },

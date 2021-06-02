8c0da36cd3bf7630543e89b49fc08b9d2e7ea9b1
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-06-02 15:23:22 +0700

    Feat: [Truyền thông] Danh sách dữ liệu thông kê cộng tác viên

--- src/graphql/modules/report/types/overviewMedia.schema.ts
+++ src/graphql/modules/report/types/overviewMedia.schema.ts
@@ -1,51 +1,63 @@
 import { gql } from "apollo-server-express";
 
 const schema = gql`
-  extend type Query{
-    getCollaboratorsMediaReports(q: QueryGetListInput): MediaCollaboratorPageData
+  extend type Query {
+    getCollaboratorsMediaReports(q: QueryGetListInput): CollaboratorPageData
     getProductsMediaReports(q: QueryGetListInput): MediaProductPageData
 
     getOverviewAllCollaboratorProducts(fromDate: String, toDate: String): OverviewMediaProductStats
     getOverviewAllCollaborators(fromDate: String, toDate: String): OverviewMediaCollaboratorStats
-    getTopMediaCollaborators(fromDate: String, toDate: String, sellerIds: [ID], branchId: ID, collaboratorId: ID): MediaCollaborators
-    getTopMediaCollaboratorProducts(fromDate: String, toDate: String,sellerIds: [ID], branchId: ID, collaboratorId: ID): MediaCollaboratorProducts
+    getTopMediaCollaborators(
+      fromDate: String
+      toDate: String
+      sellerIds: [ID]
+      branchId: ID
+      collaboratorId: ID
+    ): MediaCollaborators
+    getTopMediaCollaboratorProducts(
+      fromDate: String
+      toDate: String
+      sellerIds: [ID]
+      branchId: ID
+      collaboratorId: ID
+    ): MediaCollaboratorProducts
     # Add Query
   }
 
-  type MediaCollaborators{
+  type MediaCollaborators {
     mostLikeCollaborators: [MediaStats]
     mostShareCollaborators: [MediaStats]
     mostCommentCollaborators: [MediaStats]
     mostViewCollaborators: [MediaStats]
   }
 
-  type MediaCollaboratorProducts{
+  type MediaCollaboratorProducts {
     mostLikeProducts: [MediaStats]
     mostShareProducts: [MediaStats]
     mostCommentProducts: [MediaStats]
     mostViewProducts: [MediaStats]
   }
 
-  type MediaStats{
+  type MediaStats {
     shortUrl: String
     count: String
   }
 
-  type OverviewMediaProductStats{
+  type OverviewMediaProductStats {
     shareCount: Int
     likeCount: Int
     commentCount: Int
     completedQty: Int
   }
 
-  type OverviewMediaCollaboratorStats{
+  type OverviewMediaCollaboratorStats {
     shareCount: Int
     likeCount: Int
     commentCount: Int
     collaboratorCount: Int
   }
 
-  type MediaProductsStats{
+  type MediaProductsStats {
     "Số lần SP trong đơn chưa đặt"
     unCompletedProductsCount: Int
     "Tổng lượng SP chưa đặt"
@@ -56,14 +68,14 @@ const schema = gql`
     completedProductsQty: Int
   }
 
-  type MediaProductStats{
+  type MediaProductStats {
     "Số lượng SP chưa đặt"
     unCompletedQty: Int
     "Số lượng SP thành công"
     completedQty: Int
   }
 
-  type MediaCollaboratorStats{
+  type MediaCollaboratorStats {
     "Tổng lượt xem SP"
     productsViewCount: Int
     "Tổng lượt like SP"
@@ -76,44 +88,11 @@ const schema = gql`
     productLinksCount: Int
   }
 
-  type MediaCollaborator {
-    id: String
-    createdAt: DateTime
-    updatedAt: DateTime
-    
-    code: String
-    name: String
-    phone: String
-
-    "Link giới thiệu"
-    shortUrl: String
-    clickCount: Int
-    likeCount: Int
-    shareCount: Int
-    commentCount: Int
-
-    customerId: ID
-    memberId: ID
-
-    customer: Customer
-    member: Member
-    total:Float
-    mediaCollaboratorStats: MediaCollaboratorStats
-
-    allMediaProductsStats(fromDate: String, toDate: String): MediaProductsStats
-  }
-
-  type MediaCollaboratorPageData {
-    data: [MediaCollaborator]
-    total: Int
-    pagination: Pagination
-  }
-
   type MediaProduct {
     id: String
     createdAt: DateTime
     updatedAt: DateTime
-    
+
     "Mã cộng tác viên"
     collaboratorId: ID
     "Mã sản phẩm"
@@ -141,7 +120,6 @@ const schema = gql`
     total: Int
     pagination: Pagination
   }
-
 `;
 
 export default schema;

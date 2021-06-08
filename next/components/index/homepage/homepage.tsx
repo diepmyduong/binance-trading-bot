79bc778863ede90b4289875d48af071cfd6d63f6
Thái Việt Linh <linkthai1995@gmail.com>
2021-06-08 16:12:38 +0700

    UPDATE: config project

--- next/components/index/homepage/homepage.tsx
+++ next/components/index/homepage/homepage.tsx
@@ -1,12 +1,8 @@
-import { Banner } from "./component/banner";
 import { News } from "./component/news";
-import { QuoteHistory } from "./component/quote-history";
 
 export function Homepage() {
   return (
     <div className="z-0">
-      <Banner />
-      <QuoteHistory />
       <News />
     </div>
   );

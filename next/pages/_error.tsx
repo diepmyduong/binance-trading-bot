7f738b42814014e7288d16b506ccbca1b571701a
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-06-09 16:45:56 +0700

    ADD: [Webapp] Giao diện chi tiết đơn hàng /order/[code]

--- next/pages/_error.tsx
+++
@@ -1,9 +0,0 @@
-export default function Error({ statusCode }) {
-  return null;
-  // return <>{statusCode == 500 ? <Error500 /> : <Error404 />}</>;
-}
-
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 500;
-  return { statusCode };
-};

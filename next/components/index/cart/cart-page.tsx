1587156fdb924b34248d021620ab5680c4ac1592
Nin <nnninh.17ck1@gmail.com>
2021-06-11 20:12:24 +0700

    update: complete scroll

--- next/components/index/cart/cart-page.tsx
+++ next/components/index/cart/cart-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ListCart from "./components/list-cart";
 import Price from "../../shared/utilities/infomation/price";
 import { Button } from "../../shared/utilities/form/button";
@@ -42,6 +42,13 @@ const CartPage = () => {
     },
   ];
   const [cart, setCart] = useState(list);
+  const [totalFood, setTotalFood] = useState(0);
+  const [totalMoney, setTotalMoney] = useState(0);
+  useEffect(() => {
+    setTotalFood(cart.reduce((count, item) => (count += item.qty), 0));
+    setTotalMoney(cart.reduce((total, item) => (total += item.price * item.qty), 0));
+    // setCartProductTotal(cartProducts.length);
+  }, [cart]);
   function handleRemoveItem(index: number) {
     let newCart = cart;
     newCart.splice(index, 1);
@@ -52,8 +59,8 @@ const CartPage = () => {
       <ListCart cart={cart} onRemove={handleRemoveItem} />
       <div className="flex fixed bottom-0 left-0 w-full p-2 bg-white">
         <div className="flex-1">
-          <p>Số lượng: {cart.length} món</p>
-          <Price price={182000} />
+          <p>Số lượng: {totalFood} món</p>
+          <Price price={totalMoney} />
         </div>
         <Button className="flex-1" text="Thanh toán" primary large />
       </div>

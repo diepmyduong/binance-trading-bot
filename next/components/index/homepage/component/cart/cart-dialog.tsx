5535847579b7572bcde19a039b72e80e5901393b
Nin <nnninh.17ck1@gmail.com>
2021-06-18 11:13:20 +0700

    fix conflicts

--- next/components/index/homepage/component/cart/cart-dialog.tsx
+++ next/components/index/homepage/component/cart/cart-dialog.tsx
@@ -4,11 +4,8 @@ import { Dialog, DialogPropsType } from "../../../../shared/utilities/dialog/dia
 import ListCart from "./list-cart";
 import { Button } from "../../../../shared/utilities/form/button";
 import { NumberPipe } from "../../../../../lib/pipes/number";
-<<<<<<< HEAD
 import CustomerLoginDialog from "../../../../shared/utilities/dialog/customer-login-dialog";
 import { useShopContext } from "../../../../../lib/providers/shop-provider";
-=======
->>>>>>> da97c0e5d293dda26093bfb67f7a315dd730c28b
 import { useRouter } from "next/router";
 interface Propstype extends DialogPropsType {
   cart: Food[];
@@ -18,11 +15,8 @@ interface Propstype extends DialogPropsType {
 
 const CartDialog = (props: Propstype) => {
   const router = useRouter();
-<<<<<<< HEAD
   const { customer, cunstomerLogin } = useShopContext();
   const [showLogin, setShowLogin] = useState(false);
-=======
->>>>>>> da97c0e5d293dda26093bfb67f7a315dd730c28b
   return (
     <Dialog isOpen={props.isOpen} onClose={props.onClose} title={"Giỏ hàng của bạn"} width="320px">
       <Dialog.Body>

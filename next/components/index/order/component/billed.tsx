c969de9ce68c035dcffb548f9d967c317a0e3cc2
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-06-25 15:37:06 +0700

    Feat: [Webapp] Trang order. Item Đơn hàng Khoảng cách giữa dòng đầu với 2 dòng xa nhau quá, chỉnh lại cho đều các dòng

--- next/components/index/order/component/billed.tsx
+++ next/components/index/order/component/billed.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { HiChevronRight } from "react-icons/hi";
 import { NumberPipe } from "../../../../lib/pipes/number";
 import { Button } from "../../../shared/utilities/form/button";
@@ -25,31 +26,35 @@ export function Billed({ item, index, status }: PropsType) {
   }
   return (
     <div className="w-full mt-1 bg-white text-sm" key={index}>
-      <div className="flex items-center justify-between">
-        <div className="p-2 flex flex-col">
-          <div className="flex items-center justify-start">
-            <p className={`${style} font-bold text-sm`}>{status}</p>
-            <p className="px-2">-</p>
-            <p className="">24/12/2021</p>
-          </div>
-          <div className="flex flex-col pt-4">
-            <div className="flex items-center">
-              <p className="">{item.title}</p>
-              <p className="px-2">-</p>
-              <p className="">{item.address}</p>
+      <Link href="/order/ABC123">
+        <a href="" className="">
+          <div className="flex items-center justify-between">
+            <div className="p-2 flex flex-col">
+              <div className="flex items-center justify-start">
+                <p className={`${style} font-bold text-sm`}>{status}</p>
+                <p className="px-2">-</p>
+                <p className="">24/12/2021</p>
+              </div>
+              <div className="flex flex-col pt-1">
+                <div className="flex items-center">
+                  <p className="">{item.title}</p>
+                  <p className="px-2">-</p>
+                  <p className="">{item.address}</p>
+                </div>
+                <div className="flex items-center pt-1">
+                  <p className="font-bold">{NumberPipe(item.price)}đ</p>
+                  <p className="ml-1">(Visa/Master)</p>
+                  <p className="px-2">-</p>
+                  <p className="">{item.count} món</p>
+                </div>
+              </div>
             </div>
-            <div className="flex items-center">
-              <p className="font-bold">{NumberPipe(item.price)}đ</p>
-              <p className="ml-1">(Visa/Master)</p>
-              <p className="px-2">-</p>
-              <p className="">{item.count} món</p>
-            </div>
+            <i className="text-2xl mr-2 text-primary">
+              <HiChevronRight />
+            </i>
           </div>
-        </div>
-        <i className="text-2xl mr-2 text-primary">
-          <HiChevronRight />
-        </i>
-      </div>
+        </a>
+      </Link>
       <div className="w-full flex justify-center items-center">
         <Button
           text={`${

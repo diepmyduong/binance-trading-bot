1256f67ccb2f907ab8e2584446fec5907bb8eaf9
Nin <nnninh.17ck1@gmail.com>
2021-07-12 15:36:10 +0700

    add: trang chi tiết tài khoản

---
+++ next/pages/customer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DefaultLayout } from "../layouts/default-layout/default-layout";
+import { NextSeo } from "next-seo";
+import { CustomerPage } from "../components/index/customer/customer-page";
+
+export default function Page() {
+  return (
+    <>
+      <NextSeo title={`Trang tài khoản`} />
+      <CustomerPage />
+    </>
+  );
+}
+
+Page.Layout = DefaultLayout;

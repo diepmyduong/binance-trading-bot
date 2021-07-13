b272e72ba974be4d87f1dcc20fb1656db25b709d
Nin <nnninh.17ck1@gmail.com>
2021-07-13 09:52:23 +0700

    update: gia san pham noi bat hon

--- next/components/shared/homepage-layout/price.tsx
+++ next/components/shared/homepage-layout/price.tsx
@@ -14,11 +14,10 @@ export function Price(props: Propstype) {
         {NumberPipe(props.price, true)}
       </p>
       {(props.downPrice && (
-        <div className="flex items-center text-sm pl-2">
-          <span className={`line-through pr-2`}>{NumberPipe(props.downPrice, true)}</span>
-          {props.saleRate > 0 && (
-            <div className="bg-red-500 text-white text-xs rounded px-2">{props.saleRate || 0}%</div>
-          )}
+        <div className="flex items-center text-sm pl-2 ">
+          <span className={`line-through pr-2 font-semibold`}>
+            {NumberPipe(props.downPrice, true)}
+          </span>
         </div>
       )) ||
         ""}

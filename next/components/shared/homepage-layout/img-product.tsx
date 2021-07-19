bd2f59029042afdeeacf7ab3652c5d0aa32f3737
Nin <nnninh.17ck1@gmail.com>
2021-07-19 12:36:52 +0700

    update: product saleRate

--- next/components/shared/homepage-layout/img-product.tsx
+++ next/components/shared/homepage-layout/img-product.tsx
@@ -12,8 +12,10 @@ export function ImgProduct({ saleRate, small = false, native = false, ...props }
       {native ? <img {...props} /> : <Img {...props} />}
       {saleRate > 0 && (
         <div
-          className={`absolute -top-2 -right-2 bg-danger text-white font-semibold rounded-bl-3xl ${
-            small ? "py-1 px-2 text-sm" : "py-2 px-3 sm:p-3 sm:text-lg"
+          className={`absolute  bg-danger text-white font-semibold rounded-bl-3xl ${
+            small
+              ? "py-1 px-2 text-sm -top-1 -right-1"
+              : "-top-2 -right-2 py-2 px-3 sm:p-3 sm:text-lg"
           }`}
         >
           -<span className="pl-0.5">{saleRate}</span>%

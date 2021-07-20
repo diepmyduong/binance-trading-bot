f2d8b02a8ebf177334cf78aaeef2a7800663af0d
Nin <nnninh.17ck1@gmail.com>
2021-07-20 11:33:58 +0700

    update: cập nhật rating

---
+++ next/components/index/order-detail/components/rating-order.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { FaStar } from "react-icons/fa";
+import { HiStar } from "react-icons/hi";
+interface Propstype extends ReactProps {
+  voted: number;
+  onChange: Function;
+}
+
+export function RatingOrder(props: Propstype) {
+  const [fiveStar, setFiveStar] = useState([
+    { id: 0, checked: false },
+    { id: 1, checked: false },
+    { id: 2, checked: false },
+    { id: 3, checked: false },
+    { id: 4, checked: false },
+  ]);
+  useEffect(() => {
+    fiveStar.forEach((item) => {
+      item.id < props.voted ? (item.checked = true) : (item.checked = false);
+    });
+    setFiveStar([...fiveStar]);
+  }, [props.voted]);
+  return (
+    <div className={`flex justify-evenly main-container ${props.className || ""} `}>
+      {fiveStar.map((item) => {
+        return (
+          <i
+            className={`text-24 sm:text-40 cursor-pointer duration-300 transition-all ${
+              item.checked ? "text-yellow-400" : "text-gray-400"
+            }`}
+            onClick={() => props.onChange(item.id + 1)}
+            key={item.id}
+          >
+            <FaStar />
+          </i>
+        );
+      })}
+    </div>
+  );
+}

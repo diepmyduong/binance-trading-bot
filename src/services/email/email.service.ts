85c197cf707e61b03813cb4f1e08ef087f4f64c8
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-07-21 16:40:58 +0700

    Fix: refactor Email service

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -4,6 +4,17 @@ import nodemailer from "nodemailer";
 
 dotenv.config();
 
+const sendEmailParamSchema = {
+  from: { type: "string" },
+  to: { type: "string" },
+  subject: { type: "string" },
+  html: { type: "string" },
+  text: { type: "string", optional: true },
+  host: { type: "string", optional: true },
+  port: { type: "string", optional: true },
+  username: { type: "string", optional: true },
+  password: { type: "string", optional: true },
+};
 export default {
   name: "email",
   settings: {
@@ -14,48 +25,33 @@ export default {
   },
   actions: {
     send: {
-      params: {
-        from: { type: "string" },
-        to: { type: "string" },
-        subject: { type: "string" },
-        html: { type: "string" },
-        text: { type: "string", optional: true },
-        host: { type: "string", optional: true },
-        port: { type: "string", optional: true },
-        username: { type: "string", optional: true },
-        password: { type: "string", optional: true },
-      },
+      params: sendEmailParamSchema,
       async handler(ctx) {
-        const { from, to, subject, html, text, host, port, username, password } = ctx.params;
-        const emailTransport = await nodemailer.createTransport({
-          host: host || this.settings.host,
-          port: port || this.settings.port,
-          secure: parseInt(port || this.settings.port) === 465 ? true : false, // true for 465, false for other ports
-          auth: {
-            user: username || this.settings.username, // generated ethereal user
-            pass: password || this.settings.password, // generated ethereal password
-          },
-        });
-        return await emailTransport.sendMail({ from, to, subject, html, text });
+        return await this.sendEmail(ctx);
       },
     },
   },
-  methods: {},
   events: {
     "email.send": {
+      params: sendEmailParamSchema,
       async handler(ctx: any) {
-        const { from, to, subject, html, text, host, port, username, password } = ctx.params;
-        const emailTransport = await nodemailer.createTransport({
-          host: host || this.settings.host,
-          port: port || this.settings.port,
-          secure: parseInt(port || this.settings.port) === 465 ? true : false, // true for 465, false for other ports
-          auth: {
-            user: username || this.settings.username, // generated ethereal user
-            pass: password || this.settings.password, // generated ethereal password
-          },
-        });
-        return await emailTransport.sendMail({ from, to, subject, html, text });
+        return await this.sendEmail(ctx);
       },
+    },
+  },
+  methods: {
+    async sendEmail(ctx) {
+      const { from, to, subject, html, text, host, port, username, password } = ctx.params;
+      const emailTransport = await nodemailer.createTransport({
+        host: host || this.settings.host,
+        port: port || this.settings.port,
+        secure: parseInt(port || this.settings.port) === 465 ? true : false,
+        auth: {
+          user: username || this.settings.username,
+          pass: password || this.settings.password, // generated ethereal password
+        },
+      });
+      return await emailTransport.sendMail({ from, to, subject, html, text });
     },
   },
 } as ServiceSchema;

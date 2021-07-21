545353a029b972393243eb916349e76a0a0de6b7
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-07-21 14:42:09 +0700

    Feat: [Backend] Khởi tạo model yêu cầu đăng ký chủ shop

---
+++ src/graphql/modules/shopRegistration/shopRegistration.resolver.ts
@@ -0,0 +1,36 @@
+import { ROLES } from "../../../constants/role.const";
+import { Context } from "../../context";
+import { AddressHelper } from "../address/address.helper";
+import { MemberModel } from "../member/member.model";
+import { shopRegistrationService } from "./shopRegistration.service";
+
+const Query = {
+  getAllShopRegistration: async (root: any, args: any, context: Context) => {
+    context.auth(ROLES.ADMIN_EDITOR);
+    return shopRegistrationService.fetch(args.q);
+  },
+  getOneShopRegistration: async (root: any, args: any, context: Context) => {
+    context.auth(ROLES.ADMIN_EDITOR);
+    const { id } = args;
+    return await shopRegistrationService.findOne({ _id: id });
+  },
+};
+
+const Mutation = {
+  createShopRegistration: async (root: any, args: any, context: Context) => {
+    const { data } = args;
+    await AddressHelper.setAddress(data);
+    if (await MemberModel.findOne({ username: data.email })) throw Error("Email đã được sử dụng.");
+    if (await MemberModel.findOne({ code: data.shopCode }))
+      throw Error("Mã cửa hàng đã được sử dụng");
+    return await shopRegistrationService.create(data);
+  },
+};
+
+const ShopRegistration = {};
+
+export default {
+  Query,
+  Mutation,
+  ShopRegistration,
+};

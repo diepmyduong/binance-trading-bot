3816464ac6e6c73b5f4c088267049cc21113c3c5
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-07-21 17:15:54 +0700

    Feat: [Backend] Gửi email thông báo kích hoạt tài khoản chủ shop

--- src/graphql/modules/shopRegistration/approveShopRegis.graphql.ts
+++ src/graphql/modules/shopRegistration/approveShopRegis.graphql.ts
@@ -2,12 +2,20 @@ import { gql } from "apollo-server-express";
 import { ROLES } from "../../../constants/role.const";
 import { Context } from "../../context";
 import { MemberModel } from "../member/member.model";
-import { ShopRegistionStatus, ShopRegistrationModel } from "./shopRegistration.model";
+import {
+  IShopRegistration,
+  ShopRegistionStatus,
+  ShopRegistrationModel,
+} from "./shopRegistration.model";
 import passwordHash from "password-hash";
 import shortHash from "short-hash";
 import { ShopConfigModel } from "../shopConfig/shopConfig.model";
 import { MemberHelper } from "../member/member.helper";
 import { shopConfigService } from "../shopConfig/shopConfig.service";
+import { SettingHelper } from "../setting/setting.helper";
+import { SettingKey } from "../../../configs/settingData";
+import { UtilsHelper } from "../../../helpers";
+import LocalBroker from "../../../services/broker";
 
 export default {
   schema: gql`
@@ -61,13 +69,58 @@ export default {
               ...shopConfigService.getDefaultConfig(),
             }),
           ]);
+          await sendApproveEmail(regis, password);
           return regis;
         } else {
           regis.status = ShopRegistionStatus.REJECTED;
           regis.rejectedAt = new Date();
+          await sendRejectEmail(regis);
           return await regis.save();
         }
       },
     },
   },
 };
+async function sendApproveEmail(regis: IShopRegistration, password: any) {
+  const [title, emailTemplate, appDomain, appTitle] = await SettingHelper.loadMany([
+    SettingKey.EMAIL_REGIS_APPROVE_TITLE,
+    SettingKey.EMAIL_REGIS_APPROVE,
+    SettingKey.APP_DOMAIN,
+    SettingKey.TITLE,
+  ]);
+  const context = {
+    SHOP_NAME: regis.shopName,
+    USERNAME: regis.email,
+    PASSWORD: password,
+    DASHBOARD_LINK: `${appDomain}/shop`,
+  };
+  const { subject, html } = UtilsHelper.parseObjectWithInfo({
+    object: {
+      subject: title,
+      html: emailTemplate,
+    },
+    info: context,
+  });
+  LocalBroker.emit("email.send", { from: appTitle, to: regis.email, subject, html });
+}
+async function sendRejectEmail(regis: IShopRegistration) {
+  const [title, emailTemplate, appDomain, appTitle] = await SettingHelper.loadMany([
+    SettingKey.EMAIL_REGIS_REJECT_TITLE,
+    SettingKey.EMAIL_REGIS_REJECT,
+    SettingKey.APP_DOMAIN,
+    SettingKey.TITLE,
+  ]);
+  const context = {
+    SHOP_NAME: regis.shopName,
+    USERNAME: regis.email,
+    DASHBOARD_LINK: `${appDomain}/shop`,
+  };
+  const { subject, html } = UtilsHelper.parseObjectWithInfo({
+    object: {
+      subject: title,
+      html: emailTemplate,
+    },
+    info: context,
+  });
+  LocalBroker.emit("email.send", { from: appTitle, to: regis.email, subject, html });
+}

8cf9097228776b761df7a539fa7d36e53a612d51
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-07-22 15:41:55 +0700

    fix: ds voucher áp dụng khi chọn và màu sắc picktime và nút notification

---
+++ next/components/index/notifcation/notification.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "../../shared/utilities/spinner";
+import BreadCrumbs from "../../shared/utilities/breadcrumbs/breadcrumbs";
+import { useShopContext } from "../../../lib/providers/shop-provider";
+import { useCartContext } from "../../../lib/providers/cart-provider";
+
+export function NotificationPage() {
+  return <div className="w-full bg-white relative min-h-screen"></div>;
+}

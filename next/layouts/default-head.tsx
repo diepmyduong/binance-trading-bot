18d10938aa69e693621d211aaff957f6fe35add3
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-07-23 13:25:53 +0700

    Fix: Bắt lỗi không có shopcode

--- next/layouts/default-head.tsx
+++ next/layouts/default-head.tsx
@@ -4,13 +4,14 @@ import { useShopContext } from "../lib/providers/shop-provider";
 
 export function DefaultHead() {
   const { shopCode, shop } = useShopContext();
+
   return (
     <Head>
       <meta
         name="viewport"
         content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=0"
       />
-      <link rel="icon" type="image/png" href={shop.shopLogo || "/favicon.ico"} />
+      <link rel="icon" type="image/png" href={shop?.shopLogo || "/favicon.ico"} />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap"

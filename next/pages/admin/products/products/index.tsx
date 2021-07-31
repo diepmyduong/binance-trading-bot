562ade5e6241064a23070a6082105ee68e07d898
Thái Việt Linh <linkthai1995@gmail.com>
2021-07-31 21:51:22 +0700

    FIX: delete products page in admin

--- next/pages/admin/products/products/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Card } from "../../../../components/shared/utilities/card/card";
-import { WIP } from "../../../../components/shared/utilities/wip";
-import { AdminLayout } from "../../../../layouts/admin-layout/admin-layout";
-
-export default function Products() {
-  return <WIP />;
-}
-Products.Layout = AdminLayout;

5342fb1de969b46eed1e330562811ae2a8027683
Nin <nnninh.17ck1@gmail.com>
2021-08-03 17:18:17 +0700

    add: giao diện trang chủ

--- next/components/index/shops/shops-page.tsx
+++ next/components/index/shops/shops-page.tsx
@@ -1,3 +1,41 @@
+import React, { useState } from "react";
+import { ShopCard } from "./components/shop-card";
+import Link from "next/link";
+import { Img } from "../../shared/utilities/img";
+
 export function ShopsPage() {
-  return <div className="w-full bg-white relative min-h-screen">Danh sách cửa hàng</div>;
+  return (
+    <div className="flex flex-col min-h-screen relative bg-gray-800">
+      <header className={`fixed top-0 w-full z-100`}>
+        <div className="w-full mx-auto h-14 flex justify-between items-center max-w-lg shadow bg-white px-4">
+          <Link href={`/`}>
+            <a className="flex items-center cursor-pointer w-full">
+              <Img src="/assets/img/logo-som.png" className="w-10" />
+              <span className="text-ellipsis font-semibold px-2 text-sm sm:text-base flex-1 uppercase text-center mr-10">
+                Giaohang.shop
+              </span>
+            </a>
+          </Link>
+        </div>
+      </header>
+      <div className="w-full bg-gray-100 relative min-h-screen grid grid-cols-2 gap-4 p-4 pt-14 max-w-lg mx-auto">
+        {shops.map((item, index) => (
+          <ShopCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
+const shops = [
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+  "123123",
+];
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -10,35 +10,4 @@ export default function Page(props) {
       <ShopsPage />
     </>
   );
-  // useEffect(() => {
-  //   sessionStorage.setItem("shop", JSON.stringify(props.shop));
-  //   sessionStorage.setItem("shopCode", props.code);
-  // }, []);
-
-  // return (
-  //   <>
-  //     <HomeProvider code={props.code} shop={props.shop}>
-  //       <NextSeo title={`Trang chủ`} />
-  //       <Homepage />
-  //     </HomeProvider>
-  //   </>
-  // );
 }
-// Page.Layout = DefaultLayout;
-// export async function getServerSideProps(context: GetServerSidePropsContext) {
-//   const { code = "3MSHOP", productId } = context.query;
-//   console.log(productId);
-//   const shop = await MemberModel.findOne({ code }, "shopName shopLogo");
-//   if (!shop) {
-//     Redirect(context.res, "/not-found-shop");
-//   }
-//   return {
-//     props: JSON.parse(
-//       JSON.stringify({
-//         code,
-//         productId,
-//         shop,
-//       })
-//     ),
-//   };
-// }

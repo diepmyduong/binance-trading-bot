f08b6ce47dab9e2fd36e13a6f8fcfa0715b0bfec
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-08-04 11:39:35 +0700

    Feat: [webapp] Tự động cập nhật thông tin CTV[webapp] Tự động cập nhật thông tin CTV

--- src/graphql/modules/order/generateOrder.graphql.ts
+++ src/graphql/modules/order/generateOrder.graphql.ts
@@ -2,6 +2,7 @@ import { gql } from "apollo-server-express";
 import { set } from "lodash";
 import { ROLES } from "../../../constants/role.const";
 import { Context } from "../../context";
+import { CollaboratorModel } from "../collaborator/collaborator.model";
 import { CustomerLoader } from "../customer/customer.model";
 import { MemberLoader } from "../member/member.model";
 import { OrderGenerator } from "./orderGenerator";
@@ -48,6 +49,10 @@ export default {
               },
             })
             .exec();
+          await CollaboratorModel.updateOne(
+            { _id: customer.collaboratorId },
+            { $set: { name: customer.name } }
+          ).exec();
         }
         return order;
       },
